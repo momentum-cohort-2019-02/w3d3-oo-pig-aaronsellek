0e0e4e086f19b2b15f8aad67782938c4ea3eb82a
aaronsellek <aaron.sellek1@gmail.com>
2019-02-27 11:51:57 -0500

    commented the game

---
+++ pigs.py
@@ -0,0 +1,38 @@
+import random
+# create computer class
+# create computer
+# make it so computer rolls die returning number 1-6
+# continue rolling unless 1 is rolled or player chooses to "hold"
+# if one is rolled no points are awarded
+# dont hold unless it gets 20 points or more
+# trying to get total of 100 points
+
+# create human class
+# create human interaction
+# explain rules of game
+# roll die
+# after every roll give person chance to roll again or hold
+# if one is rolled players turn ends and gets no points
+# if player "holds" sum all points from die rollen that round and award to player
+# if player gets to 100 they win
+
+# create game class
+# greet player
+# explain rules
+# roll dice to decide who goes first
+# state points per player
+# after player rolls/holds say points
+# other player rolls, sum points if hold
+# keep switching until points >= 100
+# state player who wins by whoever gets to 100 first
+# ask player play again? if "y or yes" yes
+# if no end program
+# repeat = True
+
+# class Game:
+#     def roll_die(self):
+#         print("You rolled ", random.randint(1,6))
+#         print("Roll again? Yes, or No? ")
+#         if random.randint !=1:
+#             repeat = ("yes" or "y") in input().lower()
+        
